b59d76c76ab852add4a863a691c09f407fe03a51
Sevbas <lectoresugartinos@gmail.com>
2026-02-22 12:27:43 -0500

    Add files via upload

---
+++ Confeti.js
@@ -0,0 +1,83 @@
+// Confetis
+
+//
+// 1. Configuración Inicial
+window.oncontextmenu = function () {
+  return false;
+};
+
+const canvasConfeti = document.getElementById("canvas1"); //Lienzo
+const ctxConfeti = canvasConfeti.getContext("2d"); // Funciones
+
+let ancho = (canvasConfeti.width = window.innerWidth);
+let alto = (canvasConfeti.height = window.innerHeight);
+
+let confetis = [];
+
+const coloresConfeti = [
+  "rgba(8, 30, 92, 1)",
+  "rgba(16, 36, 151, 1)",
+  "rgba(54, 76, 173, 1)",
+  "rgba(73, 101, 192, 1)",
+  "rgba(89, 97, 204, 1)",
+  "rgba(75, 171, 201, 1)",
+  "rgba(100, 164, 184, 1)",
+];
+
+//
+// 2. Crear Confeti
+//
+function crearConfeti() {
+  const cantidad = 100;
+  for (let i = 0; i < cantidad; i++) {
+    confetis.push({
+      x: Math.random() * ancho, // Posición Horizontal Aleatoria
+      y: Math.random() * -alto, // Posición Vertical Aleatoria (Desde arriba por el negativo)
+      r: Math.random() * 5 + 2, // Radio Aleatorio (2 y 7 Pixeles)
+      color: coloresConfeti[Math.floor(Math.random() * coloresConfeti.length)],
+      velocidadY: Math.random() * 2 + 1, // Velocidad Aleatoria (1 y 3 Pixeles/Frame)
+    });
+  }
+}
+
+//
+// 3. Dibujar y Mover Confeti
+//
+function animarConfeti() {
+  ctxConfeti.fillStyle = "rgba(177, 226, 228, 1)"; // Fondo Rosa Pastel
+  ctxConfeti.fillRect(0, 0, ancho, alto);
+
+  for (let i = 0; i < confetis.length; i++) {
+    let confetiActual = confetis[i];
+
+    ctxConfeti.beginPath(); // Empezamos Una Nueva Figura
+    ctxConfeti.arc(
+      confetiActual.x,
+      confetiActual.y,
+      confetiActual.r,
+      0,
+      Math.PI * 2
+    ); // Dibujamos Un Círculo
+    ctxConfeti.fillStyle = confetiActual.color; // Color
+    ctxConfeti.fill(); // Rellenamos El Círculo
+
+    // Movimiento (Caída Libre)
+    confetiActual.y += confetiActual.velocidadY;
+
+    // Si Termina De Caer, Aparece Arriba Nuevamente
+    if (confetiActual.y > alto) {
+      confetiActual.y = -10;
+      confetiActual.x = Math.random() * ancho;
+    }
+  }
+
+  // Animación Continua (Llama de nuevo a la función)
+  requestAnimationFrame(animarConfeti);
+}
+
+crearConfeti();
+
+// Esperar 1 segundo antes de iniciar el confeti
+setTimeout(() => {
+  animarConfeti();
+}, 1500);
